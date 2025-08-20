b4606e2d817b77560e25e3801f6d129167cb756c
Candace <can.o.fisher@gmail.com>
2025-08-20 17:34:29 -0400

    Split layout into partials for easier changes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
-# config/routes.rb
 Rails.application.routes.draw do
-  get "about/index"
-  get "home/index"
-
+  # Health check endpoint
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "home#index"
 
-  get "/about", to: "about#index"
-
+  # Contact form submission
   post "/contact", to: "home#submit_contact", as: :contact
 end
-
