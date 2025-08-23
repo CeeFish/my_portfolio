a6d04d8f534d22bd16059851c7fa7da416f04052
Candace <can.o.fisher@gmail.com>
2025-08-23 01:34:07 -0400

    Added UpState to projects.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,24 @@
 class HomeController < ApplicationController
   def index
+    private_project = {
+      "name"        => "UpState (Private)",
+      "description" => "NC's fastest and most accurate legislation tracker and committee transcript tool.",
+      "url"         => "https://start.keepupstate.com/",
+      "image_urls"  => [
+        helpers.asset_path("advocacy-embed-description.png"),
+        helpers.asset_path("advocacy-embed.png")
+      ]
+    }
+
+    fetched = []
     begin
       resp = HTTParty.get("#{ENV['PY_API_URL']}/api/projects?limit=3")
-      projects = resp.success? ? resp.parsed_response : []
+      fetched = resp.success? ? resp.parsed_response : []
     rescue => e
       Rails.logger.error "Project fetch failed: #{e.message}"
-      projects = []
     end
 
-    featured = ENV.fetch("FEATURED_PROJECT", "my-portfolio")
-    @initial_projects = projects.select { |p| p["name"] == featured }
+    @initial_projects = [private_project] + fetched
   end
 
   def submit_contact
